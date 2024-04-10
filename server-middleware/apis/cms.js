b32284972e9c37e826047a0ba067865fae09dd52
caesar <caesar.lee@mirrormedia.mg>
2024-04-10 14:26:10 +0800

    refactor: comment out unused fetch

--- server-middleware/apis/cms.js
+++ server-middleware/apis/cms.js
@@ -6,33 +6,35 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const Router = require('koa-router')
-const { post: axiosPost } = require('axios')
+// const { post: axiosPost } = require('axios')
 
-const { CMS_ENDPOINT } = require('../../configs/config.js')
-const { reportApiErrorFromKoa } = require('../../helpers/index.js')
+// const { CMS_ENDPOINT } = require('../../configs/config.js')
+// const { reportApiErrorFromKoa } = require('../../helpers/index.js')
 
 const app = new Koa()
 const router = new Router()
 
 app.use(bodyParser()).use(router.routes())
 
-router.post('/', async function requestGraphqlApi(ctx) {
-  try {
-    const { data, status = 200 } = await axiosPost(
-      CMS_ENDPOINT,
-      ctx.request.body
-    )
+// router.post('/', async function requestGraphqlApi(ctx) {
+//   try {
+//     console.log(`I'm called in cms.js`)
 
-    ctx.status = status
-    ctx.body = data
-  } catch (err) {
-    reportCmsApiError(err, ctx)
-  }
-})
+//     const { data, status = 200 } = await axiosPost(
+//       CMS_ENDPOINT,
+//       ctx.request.body
+//     )
 
-function reportCmsApiError(err, koaCtx) {
-  reportApiErrorFromKoa(err, koaCtx, { scope: 'CMS' })
-}
+//     ctx.status = status
+//     ctx.body = data
+//   } catch (err) {
+//     reportCmsApiError(err, ctx)
+//   }
+// })
+
+// function reportCmsApiError(err, koaCtx) {
+//   reportApiErrorFromKoa(err, koaCtx, { scope: 'CMS' })
+// }
 
 Object.assign(module.exports, {
   handler: app.callback(),
